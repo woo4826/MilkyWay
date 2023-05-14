af4884ce31cd1a010d53d92614bc979de23524ee
sonsubin <ttokky0129@gmail.com>
2023-05-15 01:17:10 +0900

    settingsFragment layout 추가

---
+++ app/src/main/java/kr/lacto/milkyway/ui/review/ReviewFragment.kt
@@ -0,0 +1,32 @@
+package kr.lacto.milkyway.ui.review
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.lacto.milkyway.R
+
+class ReviewFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ReviewFragment()
+    }
+
+    private lateinit var viewModel: ReviewViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_review, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ReviewViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
